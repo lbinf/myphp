b2c2fab1616af574bee2a05dbd4923621cdd715a
lbinf <290887098@qq.com>
2016-09-14 12:54:09 +0800

    myphp framework init

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  'driver'    => 'mysql',
+  'host'      => '127.0.0.1',
+  'database'  => 'tinylara',
+  'username'  => 'root',
+  'password'  => '',
+  'charset'   => 'utf8',
+  'collation' => 'utf8_general_ci',
+  'prefix'    => ''
+  ];
